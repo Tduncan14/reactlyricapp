674e39fbec4b13dea66a48770b10588d8ab458e1
unknown <nbatre1@yahoo.com>
2019-01-17 20:19:48 -0600

    keep moving forward

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+ const Navbar =() => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Navbar;
+
+
+
+/*
+import React from 'react'
+
+const Navbar = {
+    return(
+         <div>
+
+
+         </div>
+    )
+}
+
+export default Navbar;
+
+
+
+*/
+ 
