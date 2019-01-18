2f75eb8075ead2ec17b90e73200485703fde6e27
unknown <nbatre1@yahoo.com>
2019-01-17 21:08:04 -0600

    keep moving forward

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Hello</h1>
-      </div>
+      <Router>
+     <React.Fragment>
+      <Navbar />
+     <div className ="container">
+       <Switch>
+        <Route exact path ="/" component={Index} />
+       </Switch>
+     </div>
+     
+     </React.Fragment>
+      </Router>
     );
   }
 }
