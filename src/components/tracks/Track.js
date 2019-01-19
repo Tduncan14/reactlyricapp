15f29ad1ca5142887dc4264aff8069f45919f425
unknown <nbatre1@yahoo.com>
2019-01-18 22:12:05 -0600

    keep moving forward

---
+++ src/components/tracks/Track.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Track = (props) =>{
+  
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Track;
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Consumer} from '../../context';
+import Spinner from '../layout/Spinner';
 
 
 class Tracks extends Component {
@@ -7,11 +8,21 @@ class Tracks extends Component {
        return(
            <Consumer>
                {value => {
-                    console.log(value);
-                    return <h1>Tracks</h1>
+               
+                    const { track_list } = value;
+
+                   if(track_list === undefined || track_list.length === 0){
+                        return <Spinner />;
+                     
+                   }else{
+                 return(   <div className="row"> 
+
+                    </div>)
+                   }
+
                }}
            </Consumer>
-       )
+       );
 
     }
 }
