4d0a02d2a3cf34546fb4a6b30911724a1b403768
unknown <nbatre1@yahoo.com>
2019-01-18 22:20:43 -0600

    keep moving forward

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Consumer} from '../../context';
 import Spinner from '../layout/Spinner';
-
+import Track from '../tracks/Track'
 
 class Tracks extends Component {
     render(){
@@ -9,15 +9,23 @@ class Tracks extends Component {
            <Consumer>
                {value => {
                
-                    const { track_list } = value;
+                    const { track_list ,heading } = value;
 
                    if(track_list === undefined || track_list.length === 0){
                         return <Spinner />;
                      
                    }else{
-                 return(   <div className="row"> 
+                 return( 
+                <React.Fragment> 
+            <h3 className ="text-center mb-4">{heading}</h3>
+                 <div className="row"> 
+                   {track_list.map(item =>(
+                       <Track />
+                   ))}
+                    </div>
+                </React.Fragment>
+                    )
 
-                    </div>)
                    }
 
                }}
