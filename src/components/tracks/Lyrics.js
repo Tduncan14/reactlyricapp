dbd083ae3b810fd79bb5ba8a00d4b73c8156c342
unknown <nbatre1@yahoo.com>
2019-01-19 12:26:59 -0600

    here are some updates and always keep moving forward:

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import axios from 'axios';
+import Spinner from '../layout/Spinner';
  class Lyrics extends Component {
+     state ={
+         track:{},
+         lyrics:{}
+     }
+
+
+     componentDidMount(){
+          axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
+          .then(res =>{
+              console.log(res.data);
+              this.setState({lyrics: res.data.message.body.lyrics});
+
+              return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`);
+
+     })
+     // might be area of problem
+      .then(res=>{
+          console.log()
+          this.setState({track:res.data.message.body.track})})
+          
+     .catch(err => console.log(err));
+
+    }
+   
+    
   render() {
-    return (
-      <div>
-          <h1>Lyrics</h1>
-        
-      </div>
-    )
+      const{track,lyrics} =this.state;
+      
+      if(track === undefined || lyrics === undefined || Object.keys(track).length === 0 || Object.keys(lyrics).length===0){
+
+       return  <Spinner />
+
+       
+      }
+
+      else{
+
+      }
+    
   }
 }
 
 
-export default Lyrics
+export default Lyrics;
 
 
 /*
