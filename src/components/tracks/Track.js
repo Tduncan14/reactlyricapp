c035b440076d2f3e0340ac322c002fa232032702
unknown <nbatre1@yahoo.com>
2019-01-18 22:44:38 -0600

    keep moving forward

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+
 const Track = (props) =>{
+    const{track} = props;
   
     return(
-        <div>
-
+        <div className="col-md-6"> 
+          <div className="card mb-4 shadow-sm">
+             <h5>{track.artist_name}</h5>
+          </div>
         </div>
     )
 }
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -20,7 +20,7 @@ class Tracks extends Component {
             <h3 className ="text-center mb-4">{heading}</h3>
                  <div className="row"> 
                    {track_list.map(item =>(
-                       <Track />
+                       <Track key={item.track.track_id}track={item.track} />
                    ))}
                     </div>
                 </React.Fragment>
