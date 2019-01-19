f17448a0fd412ad8f4b1948eddd5601d5e8071cd
unknown <nbatre1@yahoo.com>
2019-01-19 00:24:44 -0600

    keep moving forward

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+ class Lyrics extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Lyrics</h1>
+        
+      </div>
+    )
+  }
+}
+
+
+export default Lyrics
+
+
+/*
+import React,{Component} from 'react'
+
+class Lyrics extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>s
+        )
+    }
+}
+
+export default Lyrics
+*/
