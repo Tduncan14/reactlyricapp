03d729b85c3c1ea45d151323618e655ac6c382aa
unknown <nbatre1@yahoo.com>
2019-01-18 23:31:18 -0600

    keep moving forward

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 
 const Track = (props) =>{
@@ -8,6 +9,15 @@ const Track = (props) =>{
         <div className="col-md-6"> 
           <div className="card mb-4 shadow-sm">
              <h5>{track.artist_name}</h5>
+              <p className ="card-text">
+                <strong> <i className="fas fa-play">  </i> { ''} Track</strong>: {track.track_name}
+                <br />
+                <strong><i className="fas-fa-compact-disc"></i></strong> Album:{track.album_name}
+              </p>
+            <Link to={`lyrics/tracks/${track.track_id}`}className="btn btn-dark btn-block">
+               <i className="fas fa-chevron-right"></i>View Lyrics
+             </Link>
+
           </div>
         </div>
     )
