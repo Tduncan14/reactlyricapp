9d0af651fb4546295f1231cc17b5a491c7cd84da
unknown <nbatre1@yahoo.com>
2019-01-20 13:56:14 -0600

    keep moving forward always dispatcher update

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -12,13 +12,17 @@ class Search extends Component{
          this.setState({[e.target.name]: e.target.value});
      }
 
-     findTrack=(e)=>{
+     findTrack=(dispatch,e)=>{
          // to keep the from automatic submitting
          e.preventDefault();
 
          axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
          .then(res => {
              console.log(res.data);
+             dispatch({
+                 type:'SEARCH_TRACKS',
+                 payload: res.data.message.body.track_list
+             });
 
          })
          .catch(err => console.log(err));
@@ -29,13 +33,16 @@ class Search extends Component{
         return(
            <Consumer>
              {value => {
+                 // more distructing
+                 const {dispatch} = value;
+                 console.log(value);
                  return(
                      <div className="card card-body mb-4">
                        <h1 className="display-4 text-center">
                           <i className ="fas fa-music"></i> Search For A Song
                        </h1>
                        <p className="lead text-center">Get the lyrics for the song</p>
-                        <form onSubmit ={this.findTrack} >
+                        <form onSubmit ={this.findTrack.bind(this,dispatch)} >
                         <div className="form-group">
                             <input className="form-control from-control-lg" placeholder="Song Title"  name="trackTitle" value={this.state.trackTitle} onChange={this.onChange.bind(this)}/>
                             
