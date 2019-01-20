f2a3e149f47e7ff500813c1738a274699e364641
unknown <nbatre1@yahoo.com>
2019-01-19 20:20:35 -0600

    keep moving forward

---
+++ src/components/tracks/Search.js
@@ -0,0 +1,25 @@
+import React,{Component}from 'react';
+import axios from 'axios';
+import {Consumer} from '../../context';
+
+class Search extends Component{
+     state ={
+         trackTitle:''
+     }
+    render(){
+        return(
+           <Consumer>
+             {value => {
+                 return(
+                     <div className="card card-body mb-4">
+                     
+                     </div>
+                 ) 
+             }}
+           </Consumer>
+        )
+    }
+}
+
+
+export default Search;
